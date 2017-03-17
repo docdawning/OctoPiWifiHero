4399243512f384ccdefe2bed393f93b1c1d2cbdb
James <doc@dawning.ca>
2017-03-17 10:05:49 -0600

    Update startAccessPoint.sh

--- startAccessPoint.sh
+++ startAccessPoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "Prepping wlan1 for Access Point"
 
-hostapd -B /home/pi/gitlab_brainiacks__deltaflyer.git/scripts/OctopiWifiAccessPoint/hostapd.conf  > /var/log/hostapd.log 
+hostapd -B /etc/hostapd/hostapd.conf  > /var/log/hostapd.log 
 
 #ifconfig wlan1 192.168.0.1 netmask 255.255.255.0 broadcast 255.255.255.0
 
